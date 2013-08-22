c6b19dd8e2b16f50cdd1a96077f0e198262c74b1
cosmonaut <nicholas.nell@colorado.edu>
2013-08-22 11:08:00 -0600

    Basic communication with detector

--- src/tmif.c
+++ src/tmif.c
@@ -10,9 +10,36 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <fcntl.h>
+#include <signal.h>
 
 
 #define CU40MMXS_PORT 60000
+
+
+/* global loop control */
+static volatile sig_atomic_t loop_switch = 1;
+
+
+static void signal_handler(int sig) {
+    switch(sig) {
+    case SIGHUP:
+        //syslog(LOG_WARNING, "Caught signal SIGHUP! (tmif_server)");
+        loop_switch = 0;
+        break;
+    case SIGTERM:
+        //syslog(LOG_WARNING, "Caught signal SIGTERM! (tmif_server)");
+        loop_switch = 0;
+        break;
+    case SIGQUIT:
+        //syslog(LOG_WARNING, "Caught signal SIGQUIT! (tmif_server)");
+        loop_switch = 0;
+        break;
+    default:
+        //syslog(LOG_WARNING, "Caught signal (%d) %s", strsignal(sig));
+        loop_switch = 0;
+        break;
+    }
+}
 
 
 int main(void) {
@@ -25,13 +52,27 @@ int main(void) {
     int sock_status = 0;
     struct sockaddr_in sin;
     int sock_opts = 0;
+    struct sockaddr_storage from_addr;
+    socklen_t addr_len;
+
+    int sock_nbytes = 0;
+
+    /* buffers */
+    uint16_t packet_buf[2048];
+    char s[100];
+
+    /* Signals */
+    struct sigaction sa_quit;
+
 
     printf("Hello!\n");
+
+
 
     /* Create socket */
     sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock_fd < 0) {
-        printf("Error creatting socket...\n");
+        printf("Error creating socket...\n");
     }
 
     /* Bind to port 60000 */
@@ -51,7 +92,7 @@ int main(void) {
     if (sock_opts == -1) {
         printf("Failed to set socket as non-blocking\n");
     }
-    
+
 
     /* Init DM9820 board */
     dm7820_status = DM7820_General_Open_Board(0, &output_board);
@@ -64,6 +105,40 @@ int main(void) {
     if (dm7820_status < 0) {
         printf("Failed to reset board \n");
     }
+
+    /* Allow graceful quit with various signals */
+    memset(&sa_quit, 0, sizeof(sa_quit));
+    sa_quit.sa_handler = &signal_handler;
+
+    sigaction(SIGHUP, &sa_quit, NULL);
+    sigaction(SIGTERM, &sa_quit, NULL);
+    sigaction(SIGINT, &sa_quit, NULL);
+    sigaction(SIGQUIT, &sa_quit, NULL);
+
+
+    /* this is the magic. */
+    while(loop_switch) {
+    
+        sock_nbytes = recvfrom(sock_fd, packet_buf, sizeof(packet_buf), 0, (struct sockaddr *)&from_addr, &addr_len);
+        //printf("sock bytes: %i\n", sock_nbytes);
+        //perror("socket!");
+        if (sock_nbytes > 0) {
+            printf("sock bytes: %i\n", sock_nbytes);
+            printf("Num photons: %u\n", packet_buf[0]);
+            printf("Packet Count: %u\n", packet_buf[1]);
+            printf("got packet from: %s\n\n", inet_ntop(from_addr.ss_family, 
+                                                    &(((struct sockaddr_in*)((struct sockaddr *)&from_addr))->sin_addr), 
+                                                    s, 
+                                                    sizeof(s)));
+        }
+
+        usleep(50);
+    }
+
+
+
+
+    /* Close down everything gracefully */
 
     dm7820_status = DM7820_General_Close_Board(output_board);
     if (dm7820_status < 0) {
